f4db0d2c28c245893433e34cf4fd761ccfcdadc5
Marcio Barbeiro <marciobarbeiro85@gmail.com>
2025-09-09 00:08:10 -0300

    React Toastify adicionado

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,44 +1,36 @@
-import {GithubLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react";
-import { useContext, type ReactNode } from "react";
+import { InstagramLogo, LinkedinLogo, GithubLogo } from '@phosphor-icons/react'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
-  const data = new Date().getFullYear();
+  let data = new Date().getFullYear()
 
-  return (
-    <>
-      <div className="flex justify-center bg-black text-white">
+  const { usuario } = useContext(AuthContext)
+
+  let footerComponent
+
+  if (usuario.token !== '') {
+    footerComponent = (
+      <div className="flex justify-center bg-indigo-900 text-white">
         <div className="container flex flex-col items-center py-4">
-          <p className="text-xl font-bold">
-            Blog Pessoal Marcio Barbeiro | Copyright: {data}
-          </p>
-          <p className="text-lg">Acesse nossas redes sociais</p>
-          <div className="flex gap-2">
-            <a
-              href="https://www.linkedin.com/in/marciobarbeiro/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedinLogo size={48} weight="bold" />
+          <p className="text-lg font-bold">Blog Pessoal Generation | {data}</p>
+          <div className="flex gap-4 mt-2">
+            <a href="https://www.linkedin.com/in/marciobarbeiro/" target="_blank" rel="noopener noreferrer">
+              <LinkedinLogo size={32} weight="bold" />
             </a>
-            <a
-              href="https://www.instagram.com/marcio.barbeiro"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <InstagramLogo size={48} weight="bold" />
+            <a href="https://www.instagram.com/marcio.barbeiro" target="_blank" rel="noopener noreferrer">
+              <InstagramLogo size={32} weight="bold" />
             </a>
-            <a
-              href="https://github.com/marcioanttonio"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GithubLogo size={48} weight="bold" />
+            <a href="https://github.com/marcioanttonio" target="_blank" rel="noopener noreferrer">
+              <GithubLogo size={32} weight="bold" />
             </a>
           </div>
         </div>
       </div>
-    </>
-  );
+    )
+  }
+
+  return <>{footerComponent}</>
 }
 
-export default Footer;
+export default Footer
