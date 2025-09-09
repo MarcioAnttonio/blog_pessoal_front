5125e97e75fca8c5059188a6bf83b6ba444ae1f6
Marcio Barbeiro <marciobarbeiro85@gmail.com>
2025-09-08 23:09:29 -0300

    Arrumei o logo do Github no footer

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, type ReactNode } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
 
@@ -6,7 +6,7 @@ function Navbar() {
 
     const navigate = useNavigate();
 
-    const { handleLogout } = useContext(AuthContext)
+    const { usuario, handleLogout } = useContext(AuthContext)
 
     function logout() {
 
@@ -15,10 +15,14 @@ function Navbar() {
         navigate('/')
     }
 
-    return (
-        <>
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className='w-full flex justify-center py-4
-            			   bg-black text-white'>
+            			   bg-indigo-900 text-white'>
             
                 <div className="container flex justify-between text-lg mx-8">
                     <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
@@ -32,6 +36,13 @@ function Navbar() {
                     </div>
                 </div>
             </div>
+
+        )
+    }
+
+    return (
+        <>
+            { component }
         </>
     )
 }
